15c5627d47e72a4b89a5b0d4015e0b36ab500d81
dk-17 <68947238+dk-17@users.noreply.github.com>
2021-02-27 07:38:56 +0530

    Create 1656. Design an Ordered Stream.cpp

---
+++ Weekly_Contest/215/1656. Design an Ordered Stream.cpp
@@ -0,0 +1,26 @@
+class OrderedStream {
+    
+public:
+    vector<string> stream;
+    int i=0;
+    OrderedStream(int n) {
+        stream.resize(n);
+        
+    }
+    
+    vector<string> insert(int id, string value) {
+        vector<string> result;
+        stream[id-1]=value;
+        while(i<stream.size()&& stream[i]!=""){
+            result.push_back(stream[i]);
+            i++;
+        }
+        return result;
+    }
+};
+
+/**
+ * Your OrderedStream object will be instantiated and called as such:
+ * OrderedStream* obj = new OrderedStream(n);
+ * vector<string> param_1 = obj->insert(id,value);
+ */
