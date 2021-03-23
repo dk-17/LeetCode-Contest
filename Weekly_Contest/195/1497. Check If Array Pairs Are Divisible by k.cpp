32d89fd5fa6331c3d5f5380f61762d52c3541c37
dk-17 <68947238+dk-17@users.noreply.github.com>
2021-03-23 10:18:28 +0530

    a % k = x;

---
+++ Weekly_Contest/195/1497. Check If Array Pairs Are Divisible by k.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        unordered_map<int,int> umap;
+        for(int i = 0; i < arr.size(); i++){
+             int rem = arr[i] % k;
+            
+            if(rem < 0) rem +=k;
+            umap[rem]++; 
+        }
+       
+        for(int i = 0; i < arr.size(); i++){
+            int rem =  arr[i] % k;
+            if(rem < 0) rem +=k;
+         
+            if(rem == 0 && (umap[rem] % 2 ) != 0)return false;
+             else if(rem != 0 && umap[rem] != umap[k - rem])   return false;
+            
+        }
+        return true;
+        
+    }
+};
