e48bc997a0a7c90b645c5d166508727abc1dde05
dk-17 <68947238+dk-17@users.noreply.github.com>
2021-04-04 20:16:09 +0530

    Binary_search!

--- Weekly_Contest/235/5724. Minimum Absolute Sum Difference.cpp
+++ Weekly_Contest/235/5724. Minimum Absolute Sum Difference.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
     int minAbsoluteSumDiff(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans;
+       long long int ans = 0;
         int mod = 1e9 + 7;
-        for(int i = 0 ; i< nums1.size(); i++){
-            ans.push_back(abs(nums1[i]- nums2[i]));
+        int n =nums1.size();
+        vector<int> diff;
+        for(int i= 0; i < nums1.size(); i++){
+            diff.push_back(abs(nums1[i] - nums2[i]));
+            ans+=diff[i];
         }
-        int index = 0;
-        int max_val = ans[0];
-        for(int i = 1; i < nums1.size(); i++){
-            if(max_val < ans[i]){
-                max_val = ans[i];
-                index = i;
-            }
+     
+        vector<int> arr(nums1.size());
+        sort(nums1.begin(),nums1.end());
+        for(int i = 0; i < nums1.size(); i++){
+            int j  = lower_bound(nums1.begin(),nums1.end(),nums2[i]) - nums1.begin();
+            if(j!=0 && j != n)
+                arr[i] = min(abs(nums2[i]- nums1[j]),abs(nums2[i]- nums1[j-1]));
+            else if(j == 0)
+                arr[i] = abs(nums2[i] - nums1[j]);
+            else
+                arr[i] = abs(nums2[i] - nums1[j-1]);
         }
         
-      //  cout<<"index= "<<index<<endl;
-        int add = INT_MAX;
-        for(int i = 0; i < nums1.size(); i++){
-            add= min(add,abs(nums2[index] - nums1[i]));
-        }
-        long long int total = 0;
-        for(int i =0; i< ans.size(); i++){
-            if(i == index)
-                total+=add;
-            else
-                total+=ans[i];
-        }
-        return total % mod;
+        int best  = 0;
+        for(int i = 0; i< arr.size(); i++)
+            best = max(best,diff[i]-arr[i]);
+    
+        return (ans-best) % mod;
         
     }
 };
