52d97f34da08651ed63612b4cec04e2d2744d4ed
dk-17 <68947238+dk-17@users.noreply.github.com>
2021-04-14 07:51:29 +0530

    prefix sum

---
+++ Weekly_Contest/193/1480. Running Sum of 1d Array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int sum = 0; 
+        vector<int> ans;
+        ans.push_back(nums[0]);
+        sum = nums[0];
+        for(int i = 1; i < nums.size(); i++){
+            sum+=nums[i];
+            ans.push_back(sum);
+        }
+        return ans;
+        
+    }
+};
